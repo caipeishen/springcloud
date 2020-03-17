c905464ca86df18955cf101395829ea1da9a8cc8
Mr丶Cps <peishen.cai@foxmail.com>
2020-03-17 14:52:22 +0800

    8001提供者 8002消费者

---
+++ springcloud-web-consumer-8002/src/main/java/cn/bd/springcloud/Application_8002.java
@@ -0,0 +1,13 @@
+package cn.bd.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application_8002
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application_8002.class, args);
+    }
+}
