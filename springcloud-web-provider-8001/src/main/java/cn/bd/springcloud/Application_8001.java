36e798f407985eace5eb2db687341b5a1d7452f3
Mr丶Cps <peishen.cai@foxmail.com>
2020-03-17 17:04:36 +0800

    Eureka注册与发现完成

--- springcloud-web-provider-8001/src/main/java/cn/bd/springcloud/Application_8001.java
+++ springcloud-web-provider-8001/src/main/java/cn/bd/springcloud/Application_8001.java
@@ -5,19 +5,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@MapperScan("cn.bd.springcloud.dao")  //扫描dao层
-@SpringBootApplication      //SpringBoot启动2
-public class Application_8001 extends SpringBootServletInitializer {
+
+@EnableEurekaClient //服务注册到Eureaka
+@EnableDiscoveryClient //服务发现Eureaka
+@MapperScan("cn.bd.springcloud.dao")
+@SpringBootApplication
+public class Application_8001{
 
     public static void main(String[] args) {
         SpringApplication.run(Application_8001.class, args);
-    }
-
-    //重写configure方法
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application_8001.class);
     }
 
 }
--- springcloud-web-provider-8001/src/main/java/cn/bd/springcloud/web/DeptController_8001.java
+++ springcloud-web-provider-8001/src/main/java/cn/bd/springcloud/web/DeptController_8001.java
@@ -2,11 +2,12 @@ package cn.bd.springcloud.web;
 
 import cn.bd.springcloud.entity.Dept;
 import cn.bd.springcloud.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
 import javax.annotation.Resource;
 import java.util.List;
 
@@ -17,6 +18,9 @@ public class DeptController_8001 {
 
     @Resource
     DeptService userService;
+
+    @Autowired
+    private DiscoveryClient client;
 
     @RequestMapping("/add")
     public Integer addDept(Dept dept){
@@ -31,6 +35,24 @@ public class DeptController_8001 {
     @RequestMapping("/list")
     public List<Dept> getDeptList(){
         return userService.getDeptList();
+    }
+
+    /**
+     * 服务发现
+     * @return
+     */
+    @RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+    public Object discovery()
+    {
+        List<String> list = client.getServices();
+        System.out.println("**********" + list);
+
+        List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
+        for (ServiceInstance element : srvList) {
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+                    + element.getUri());
+        }
+        return this.client;
     }
 
 }
