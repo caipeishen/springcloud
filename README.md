8b64c4aa01c765cb8bd7a59c9bafd3b997e855fa
Mr丶Cps <peishen.cai@foxmail.com>
2020-03-29 09:49:03 +0800

    添加备注

---
+++ README.md
@@ -0,0 +1,20 @@
+
+
+### 数据库文件
+
+```sql
+cloud_db_01.sql、cloud_db_02.sql、cloud_db_03.sql
+```
+
+
+
+### 本机映射
+
+```
+127.0.0.1	eureka7001.com
+127.0.0.1	eureka7002.com
+127.0.0.1	eureka7003.com
+
+127.0.0.1	myzuul9527.com
+```
+
---
+++ sql/cloud_db_01.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Ultimate v8.32 
+MySQL - 5.1.73 : Database - cloud_db_01
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`cloud_db_01` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `cloud_db_01`;
+
+/*Table structure for table `dept` */
+
+DROP TABLE IF EXISTS `dept`;
+
+CREATE TABLE `dept` (
+  `dno` int(11) NOT NULL AUTO_INCREMENT,
+  `dname` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `db_source` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`dno`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+/*Data for the table `dept` */
+
+insert  into `dept`(`dno`,`dname`,`db_source`) values (1,'人事部','cloud_bd_01'),(2,'开发部','cloud_bd_01'),(3,'工程部','cloud_bd_01'),(4,'行政部','cloud_bd_01'),(5,'啊实打实打算','cloud_bd_01'),(6,NULL,'cloud_db_01');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
